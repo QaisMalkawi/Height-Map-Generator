9506178fc7ea56ecdcf9e794c7d26fba7c8be351
QaisMalkawi <77750632+QaisMalkawi@users.noreply.github.com>
2022-07-24 22:18:56 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace Height_Map_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.RedChannel = new System.Windows.Forms.CheckBox();
+            this.GreenChannel = new System.Windows.Forms.CheckBox();
+            this.BlueChannel = new System.Windows.Forms.CheckBox();
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.Browse = new System.Windows.Forms.Button();
+            this.InputPicture = new System.Windows.Forms.PictureBox();
+            this.GenerateImage = new System.Windows.Forms.Button();
+            this.OutputPicture = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.TickTimer = new System.Windows.Forms.Timer(this.components);
+            this.InverseChannels = new System.Windows.Forms.CheckBox();
+            this.Save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.InputPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RedChannel
+            // 
+            this.RedChannel.AutoSize = true;
+            this.RedChannel.Location = new System.Drawing.Point(12, 13);
+            this.RedChannel.Name = "RedChannel";
+            this.RedChannel.Size = new System.Drawing.Size(45, 17);
+            this.RedChannel.TabIndex = 0;
+            this.RedChannel.Text = "Red";
+            this.RedChannel.UseVisualStyleBackColor = true;
+            // 
+            // GreenChannel
+            // 
+            this.GreenChannel.AutoSize = true;
+            this.GreenChannel.Location = new System.Drawing.Point(63, 13);
+            this.GreenChannel.Name = "GreenChannel";
+            this.GreenChannel.Size = new System.Drawing.Size(55, 17);
+            this.GreenChannel.TabIndex = 1;
+            this.GreenChannel.Text = "Green";
+            this.GreenChannel.UseVisualStyleBackColor = true;
+            // 
+            // BlueChannel
+            // 
+            this.BlueChannel.AutoSize = true;
+            this.BlueChannel.Location = new System.Drawing.Point(124, 12);
+            this.BlueChannel.Name = "BlueChannel";
+            this.BlueChannel.Size = new System.Drawing.Size(46, 17);
+            this.BlueChannel.TabIndex = 2;
+            this.BlueChannel.Text = "Blue";
+            this.BlueChannel.UseVisualStyleBackColor = true;
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(12, 36);
+            this.filePath.Name = "filePath";
+            this.filePath.ReadOnly = true;
+            this.filePath.Size = new System.Drawing.Size(680, 20);
+            this.filePath.TabIndex = 3;
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(698, 33);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 23);
+            this.Browse.TabIndex = 4;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.BrowseClicked);
+            // 
+            // InputPicture
+            // 
+            this.InputPicture.Cursor = System.Windows.Forms.Cursors.No;
+            this.InputPicture.Location = new System.Drawing.Point(12, 62);
+            this.InputPicture.Name = "InputPicture";
+            this.InputPicture.Size = new System.Drawing.Size(480, 270);
+            this.InputPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.InputPicture.TabIndex = 5;
+            this.InputPicture.TabStop = false;
+            // 
+            // GenerateImage
+            // 
+            this.GenerateImage.Location = new System.Drawing.Point(498, 62);
+            this.GenerateImage.Name = "GenerateImage";
+            this.GenerateImage.Size = new System.Drawing.Size(75, 271);
+            this.GenerateImage.TabIndex = 6;
+            this.GenerateImage.Text = "Generate";
+            this.GenerateImage.UseVisualStyleBackColor = true;
+            this.GenerateImage.Click += new System.EventHandler(this.GenerateImage_Click);
+            // 
+            // OutputPicture
+            // 
+            this.OutputPicture.Cursor = System.Windows.Forms.Cursors.No;
+            this.OutputPicture.Location = new System.Drawing.Point(579, 63);
+            this.OutputPicture.Name = "OutputPicture";
+            this.OutputPicture.Size = new System.Drawing.Size(480, 270);
+            this.OutputPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.OutputPicture.TabIndex = 7;
+            this.OutputPicture.TabStop = false;
+            this.OutputPicture.Click += new System.EventHandler(this.OutputPicture_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // TickTimer
+            // 
+            this.TickTimer.Enabled = true;
+            this.TickTimer.Interval = 50;
+            this.TickTimer.Tick += new System.EventHandler(this.Tick);
+            // 
+            // InverseChannels
+            // 
+            this.InverseChannels.AutoSize = true;
+            this.InverseChannels.Location = new System.Drawing.Point(176, 13);
+            this.InverseChannels.Name = "InverseChannels";
+            this.InverseChannels.Size = new System.Drawing.Size(63, 17);
+            this.InverseChannels.TabIndex = 8;
+            this.InverseChannels.Text = "Inverse";
+            this.InverseChannels.UseVisualStyleBackColor = true;
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(779, 34);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 9;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.SaveResult);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1072, 344);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.InverseChannels);
+            this.Controls.Add(this.OutputPicture);
+            this.Controls.Add(this.GenerateImage);
+            this.Controls.Add(this.InputPicture);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.BlueChannel);
+            this.Controls.Add(this.GreenChannel);
+            this.Controls.Add(this.RedChannel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Height Map Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.InputPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox RedChannel;
+        private System.Windows.Forms.CheckBox GreenChannel;
+        private System.Windows.Forms.CheckBox BlueChannel;
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.PictureBox InputPicture;
+        private System.Windows.Forms.Button GenerateImage;
+        private System.Windows.Forms.PictureBox OutputPicture;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer TickTimer;
+        private System.Windows.Forms.CheckBox InverseChannels;
+        private System.Windows.Forms.Button Save;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Height_Map_Generator
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap res;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Tick(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(filePath.Text) || string.IsNullOrWhiteSpace(filePath.Text))
+            {
+                GenerateImage.Hide();
+            }
+            else
+            {
+                GenerateImage.Show();
+            }
+        }
+
+
+        private void BrowseClicked(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Png file|*.png|Jpg file|*.jpg";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                filePath.Text = ofd.FileName;
+
+                InputPicture.Image = new Bitmap(filePath.Text);
+            }
+        }
+
+        private void GenerateImage_Click(object sender, EventArgs e)
+        {
+            Bitmap original = new Bitmap(filePath.Text);
+            Bitmap result = new Bitmap(InputPicture.Image.Width, InputPicture.Image.Height);
+
+            
+            for (int x = 0; x < result.Width; x++)
+            {
+                for (int y = 0; y < result.Height; y++)
+                {
+
+                    float value = 0;
+
+                    int divider = 0;
+                    if (RedChannel.Checked)
+                    {
+                        value += original.GetPixel(x, y).R;
+                        divider++;
+                    }
+                    if (GreenChannel.Checked)
+                    {
+                        value += original.GetPixel(x, y).G;
+                        divider++;
+                    }
+                    if (BlueChannel.Checked)
+                    {
+                        value += original.GetPixel(x, y).B;
+                        divider++;
+                    }
+
+                    if (divider == 0)
+                    {
+                        System.Media.SystemSounds.Exclamation.Play();
+                        return;
+                    }
+
+                    if (!InverseChannels.Checked)
+                    {
+                        Color pixelColor = Color.FromArgb(255 - ((int)value / divider), 255 - ((int)value / divider), 255 - ((int)value / divider));
+                        result.SetPixel(x, y, pixelColor);
+                    }
+                    else
+                    {
+                        Color pixelColor = Color.FromArgb((int)value / divider, (int)value / divider, (int)value / divider);
+                        result.SetPixel(x, y, pixelColor);
+                    }
+                }
+            }
+
+            OutputPicture.Image = result;
+            res = result;
+        }
+        private void OutputPicture_Click(object sender, EventArgs e)
+        {
+            SaveResult(sender, e);
+        }
+        private void SaveResult(object sender, EventArgs e)
+        {
+            if (OutputPicture.Image != null)
+            {
+
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "Png file|*.png|Jpg file|*.jpg";
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    res.Save(sfd.FileName);
+
+                }
+            }
+            else
+            {
+                System.Media.SystemSounds.Exclamation.Play();
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
